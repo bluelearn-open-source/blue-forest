9b45c95287c8b387c3fb00c87dd2f2e0318dbd46
YogPanjarale <yog.panjarale@gmail.com>
2021-07-02 00:04:42 +0530

    worked on leaderboard but img's still borken

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,8 @@
-import Head from 'next/head'
 import Logo from '../components/logo'
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Blue Forest</title>
-        <link rel="icon" href="/favicon.ico" />
-        <meta charSet="utf-8"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1"/>
-    <meta name="description" content="blueforest is a study pomodoro app made for study buddies of bluelearn community"/>
-    <meta name="robots" content="index, follow"/>
-    <meta name="twitter:title" content="blueforest"/>
-    <meta name="google-site-verification" content="csoI53o6KYExBmUUJm52ae-G-NVYVeu3rvJui54sbvo" />
-    <meta name="twitter:description"
-        content="blueforest is a study pomodoro app made for study buddies of bluelearn community"/>
-
-    <link rel="canonical" href="https://clinifyforest.herokuapp.com/" />
-      </Head>
-
+     
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
         <h1 className="text-4xl font-bold">
           Welcome to{' '}
---
+++ pages/leaderboard.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import LeaderBoardItem from '../components/leaderboardItem';
+import { Person } from '../utils/types';
+function LeaderBoard({ people }: { people: Person[] }) {
+    return (
+        <div className="flex flex-col ">
+			{people.map((p,i)=>
+		
+				(
+
+           <LeaderBoardItem key={"this will work"+i} rank={i} {...p}/> 
+				))}
+        </div>
+    )
+}
+
+LeaderBoard.propTypes = {
+
+}
+
+export default LeaderBoard
+
+
+export async function getServerSideProps({ params }) {
+	const req = await fetch(
+		`https://clinifyforest.herokuapp.com/clinifyforest/api/getlb/10`
+		,
+	);
+	let data: Person[] = await req.json();
+	console.log(data);
+	
+	// converting profile pic id to link
+	data= data.map((d)=>{
+		console.log(d.id,d.avatar);
+		
+		d.avatar= `https://cdn.discordapp.com/avatars/${d.id}/${d.avatar}.png`
+		
+		return d
+	})
+	// console.log(d/);
+	return {
+		props: { people: data },
+	};
+}
