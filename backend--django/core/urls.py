0283e1f0e2f19ced23c2cb6ff66915758ecc46f3
Divyansh Agarwal <divyanshagarwal80@gmail.com>
2021-07-05 14:43:30 +0530

    testingg

--- backend--django/core/urls.py
+++ backend--django/core/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path
 from graphene_django.views import GraphQLError, GraphQLView
 
+#testingg
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('graphql', GraphQLView.as_view(graphiql=True)),
