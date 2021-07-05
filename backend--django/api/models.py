5eaf0125bfa3bc2da1f8305457d4aee0f9701ae1
Divyansh Agarwal <divyanshagarwal80@gmail.com>
2021-07-05 21:41:49 +0530

    Made Basic tree model- graphql querry-to fetch all trees mutation to add a tree

--- backend--django/api/models.py
+++ backend--django/api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
+from django.utils.translation import deactivate
 
 class User(models.Model):
     discord_id = models.BigIntegerField(primary_key=True ,unique=True)
@@ -10,10 +11,39 @@ class User(models.Model):
     coins = models.PositiveIntegerField(default=0)
     tress_unlocked = ArrayField(models.IntegerField())
 
+    def __str__(self):
+        return self.discord_tag
+
+
+class Tree(models.Model):
+    tree_id = models.IntegerField(primary_key=True,unique=True)
+    tree_name = models.CharField(max_length=100)
+    tree_url = models.CharField(max_length=100)
+    price = models.IntegerField(default=0)
+    xp_plant = models.IntegerField(default=0)
+    time_plant = models.IntegerField(default=0)
+    VERSION = (
+        ("V1", "Version1"),
+        ("V2", "Version2"),
+        ("V3", "Version3"),
+    )
+    version_level = models.CharField(choices=VERSION, max_length=255)
+
+    def __str__(self):
+        return self.tree_name
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 
 # Create your models here.
-def __str__(self):
-    return self.discord_tag
--- backend--django/api/schema.py
+++ backend--django/api/schema.py
@@ -1,24 +1,44 @@
 
+from itertools import Predicate
 import graphene
+from graphene.types import field
 from graphene_django import DjangoObjectType, DjangoListField
-from .models import User
+from .models import Tree, User
 
 class userType(DjangoObjectType):
     class Meta:
         model = User
         field = "__all__"
 
+class treeType(DjangoObjectType):
+    class Meta:
+        model = Tree
+        field = "__all__"
 
 
 class Query(graphene.ObjectType):
     all_users = graphene.List(userType)
     one_user = graphene.Field(userType, user_id= graphene.Int())
+    all_tress = graphene.List(treeType)
+    tree = graphene.Field(treeType, tree_id=graphene.Int())
 
     def resolve_all_users(self, info, **kwargs):
         return User.objects.all()
 
     def resolve_user(self, info, user_id):
         return User.objects.get(pk=user_id)
+
+    def resolve_all_tress(self, info, **kwargs):
+        return Tree.objects.all()
+
+    def resolve_tree(self, info, tree_id):
+        return Tree.objects.get(pk=tree_id)
+
+
+
+
+
+
 
 class UserInput(graphene.InputObjectType):
     id = graphene.ID()
@@ -29,6 +49,39 @@ class UserInput(graphene.InputObjectType):
     xp = graphene.Int()
     coins = graphene.Int()
     tress_unlocked = graphene.List(graphene.Int)
+
+class TreeInput(graphene.InputObjectType):
+    id = graphene.ID()
+    tree_id = graphene.Int()
+    tree_name = graphene.String()
+    tree_url = graphene.String()
+    price = graphene.Int()
+    xp_plant = graphene.Int()
+    time_plant = graphene.Int()
+    version_level = graphene.String()
+
+
+class CreateTree(graphene.Mutation):
+    class Arguements:
+        tree_data = TreeInput(required = True )
+    
+    tree = graphene.Field(treeType)
+
+    @staticmethod
+    def mutate(root, info , tree_data = None):
+        tree_instance = Tree(
+            tree_id = tree_data.tree_id,
+            tree_name = tree_data.tree_name,
+            tree_url = tree_data.tree_url,
+            price = tree_data.price,
+            xp_plant = tree_data.xp_plant,
+            time_plant = tree_data.time_plant,
+            version_level = tree_data.version_level,
+           )
+
+        tree_instance.save()
+        return CreateTree(tree = tree_instance)
+
 
 class CreateUser(graphene.Mutation):
     class Arguements:
@@ -52,6 +105,20 @@ class CreateUser(graphene.Mutation):
         user_instance.save()
         return CreateUser(user=user_instance)
 
+class DeleteTree(graphene.Mutation):
+    class Arguements:
+        id = graphene.ID()
+
+        tree = graphene.Field(treeType)
+
+        @staticmethod
+        def mutate(root, info , id):
+            tree_instance = Tree.objects.get(pk = id)
+            tree_instance.delete()
+
+            return None
+
+
 class DeleteUser(graphene.Mutation):
     class Arguements:
         id = graphene.ID()
@@ -69,6 +136,9 @@ class DeleteUser(graphene.Mutation):
 class Mutation(graphene.ObjectType):
     create_user = CreateUser.Field()
     delete_user = DeleteUser.Field()
+    create_tree = CreateTree.Field()
+    delete_tree = DeleteTree.Field()
+
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
 
