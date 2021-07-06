d49ee025c950df00e24c508cea39ea56bee26b1a
YogPanjarale <yog.panjarale@gmail.com>
2021-07-06 18:11:17 +0530

    closes #17 by the last commit

--- next-js-app/components/feedItem.tsx
+++ next-js-app/components/feedItem.tsx
@@ -11,11 +11,10 @@ export default function FeedItem({
 		<div className="h-11 inline-flex space-x-5  max-w-xs mb-4 items-center justify-center">
 			<div className="inline-flex flex-col space-y-1.5 items-start justify-start flex-1 h-full">
 				<p className="w-full h-full text-base leading-tight tracking-normal text-gray-600">
-
-                        <div className="w-52 font-Mulish">
-                            <span className="font-black">{userName}</span> is{" "}
-                            {desciption.toLowerCase()}
-                        </div>
+					<div className="w-52 font-Mulish">
+						<span className="font-black">{userName}</span> is{" "}
+						{desciption.toLowerCase()}
+					</div>
 				</p>
 				<div className="w-32 h-0.5 border border-indigo-50 bg-indigo-50" />
 			</div>
