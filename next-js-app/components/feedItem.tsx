b570f00519ce87a66b76d35638a74f9f687d13f4
YogPanjarale <yog.panjarale@gmail.com>
2021-07-09 12:55:57 +0530

    fixed bottom line and fixes #11

--- next-js-app/components/feedItem.tsx
+++ next-js-app/components/feedItem.tsx
@@ -8,18 +8,18 @@ export default function FeedItem({
 }: FeedItemProp) {
 	const timeString = dateToTimeAgo(timeStamp);
 	return (
-		<div className="h-11 inline-flex space-x-5  max-w-xs mb-4 items-center justify-center">
+		<div className="h-12 inline-flex space-x-5  max-w-xs mb-4 items-center justify-center">
 			<div className="inline-flex flex-col space-y-1.5 items-start justify-start flex-1 h-full">
-				<p className="w-full h-full text-base leading-tight tracking-normal text-gray-600">
+				<div className="w-full h-full text-base leading-tight tracking-normal text-gray-600">
 					<div className="w-52 font-Mulish">
 						<span className="font-black">{userName}</span> is{" "}
 						{desciption.toLowerCase()}
 					</div>
-				</p>
-				<div className="w-32 h-0.5 border border-indigo-50 bg-indigo-50" />
+				</div>
+				<div className="w-24 h-0.5 border border-indigo-100 bg-indigo-100"></div>
 			</div>
 			<p className="self-start pt-0.5 text-xs  font-semibold tracking-wider text-right text-indigo-900 text-opacity-25">
-				{timeString}
+				{timeString} 
 			</p>
 		</div>
 	);
