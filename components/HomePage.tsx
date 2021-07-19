5babbcb82e9b004fa7ad2b81bc1e5af8cc4305a4
Yog <yog.panjarale@gmail.com>
2021-07-19 12:22:00 +0530

    made logo absolute

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -1,48 +1,49 @@
-import React, { useState } from 'react'
-import Logo from '../components/logo'
-import OnlinePeopleList from '../components/onlinePeopleList'
-import { Button } from './button'
-import Feed from './feedList'
-import LeaderBoard from './leaderboardList'
+import React, { useState } from "react";
+import Logo from "../components/logo";
+import OnlinePeopleList from "../components/onlinePeopleList";
+import { Button } from "./button";
+import Feed from "./feedList";
+import LeaderBoard from "./leaderboardList";
 
-
-
-export default function Home() {
-    const [leaderBoardVisible, _setLeaderBoardVisible] = useState(true);
-    const [feedVisible, _setFeedVisible] = useState(false);
-    const setLeaderBoardVisible = (v=true)=>{
-        _setLeaderBoardVisible(v);
-        _setFeedVisible(!v);
-    }
-    const setFeedVisible = (v=true)=>{
-        _setFeedVisible(v);
-        _setLeaderBoardVisible(!v);
-    }
-    return (
-        <div className='flex flex-row'>
-            <div className='p-2 '>
-
-<Logo mode='medium'/>
-</div>
-        <div className=" w-full flex flex-row justify-end p-4 ">
-            
-            <div className='flex flex-col mr-10 w-80 '>
-
-                <div className='flex flex-row justify-evenly p-2 '>
-
-                <Button onPressed={()=>setFeedVisible()} title="Feed" active={feedVisible}></Button>
-                <Button onPressed={()=>setLeaderBoardVisible()} title="Leaderboard" active={leaderBoardVisible}></Button>
-                </div>
-                <div>
-            {leaderBoardVisible?<LeaderBoard/>:null}
-            {feedVisible?<Feed/>:null}
-                </div>
-            </div>
-            <div className='h-full py-2'>
-
-            <OnlinePeopleList />
-            </div>
-        </div>
-        </div>
-    )
+export default function HomePage() {
+	const [leaderBoardVisible, _setLeaderBoardVisible] = useState(true);
+	const [feedVisible, _setFeedVisible] = useState(false);
+	const setLeaderBoardVisible = (v = true) => {
+		_setLeaderBoardVisible(v);
+		_setFeedVisible(!v);
+	};
+	const setFeedVisible = (v = true) => {
+		_setFeedVisible(v);
+		_setLeaderBoardVisible(!v);
+	};
+	return (
+		<div className="flex flex-row">
+			<div className="py-4 px-8 absolute ">
+				<Logo mode="medium" />
+			</div>
+			<div className=" w-full flex flex-row justify-end p-4 ">
+				<div className="flex flex-col mr-10 w-80 ">
+					<div className="flex flex-row justify-evenly p-2 ">
+						<Button
+							onPressed={() => setFeedVisible()}
+							title="Feed"
+							active={feedVisible}
+						></Button>
+						<Button
+							onPressed={() => setLeaderBoardVisible()}
+							title="Leaderboard"
+							active={leaderBoardVisible}
+						></Button>
+					</div>
+					<div>
+						{leaderBoardVisible ? <LeaderBoard /> : null}
+						{feedVisible ? <Feed /> : null}
+					</div>
+				</div>
+				<div className="h-full py-2">
+					<OnlinePeopleList />
+				</div>
+			</div>
+		</div>
+	);
 }
