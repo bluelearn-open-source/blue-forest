58f36faa0481ea122ffba9415e3750d06d887470
Yog <yog.panjarale@gmail.com>
2021-07-19 12:44:09 +0530

    added  google signin

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react'
+// import AuthScreen from '../components/AuthScreen';
 import Home from '../components/HomePage';
 
 
 export default function HomePage() {
+    // return <AuthScreen/>
    return (<Home/>);
 }
